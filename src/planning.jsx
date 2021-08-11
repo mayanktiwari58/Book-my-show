ed38e13bbf8e93ad75535bbd02bf427d64ea6075
mayanktiwari58 <76210281+mayanktiwari58@users.noreply.github.com>
2021-08-11 23:31:09 +0530

    Add files via upload

---
+++ src/planning.jsx
@@ -0,0 +1,22 @@
+//Book my show  planning
+
+//-> we nedd a resuable carousle
+//-> hero carousle
+//-> common carousle
+//-> cast and crew small carousle
+
+// pages 
+//-> home pages
+//-> movie pages
+//-> categories pages
+//-> auth pages
+//-> razporapy payment gateway
+
+// plan -> How are we building it
+// Layouts
+// Routes
+// pages
+// Components
+
+// HOC
+// higher order components
